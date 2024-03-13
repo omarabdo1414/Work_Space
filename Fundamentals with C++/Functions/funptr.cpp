12ca93853a6088ed0b91852d18dbc39b41b3e0f5
omarabdo1414 <oabdo6063@gmail.com>
2024-03-13 23:48:11 +0200

    ..

--- Fundamentals with C++/Functions/funptr.cpp
+++ Fundamentals with C++/Functions/funptr.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 #include <array>
 #include <functional>
-
-void ForEach(const std::array<int, 6>& values, std::function<void(int)>func)
-{
-    for(auto value : values)
-        func(value);
-}
+#include <algorithm>
 
 int main()
 {
     std::array<int, 6> arr = {1,2,3,4,5,6};
-    int i = 5;
-    ForEach(arr, [&](int val){
-        std::cout << val * i << std::endl;
-    });
+
+    auto lambda = [](int i)
+    {
+        return i > 3;
+    };
+    auto result = std::find_if(arr.begin(), arr.end(), lambda);
+
+    std::cout << *result << std::endl;
 
     return 0;
 }

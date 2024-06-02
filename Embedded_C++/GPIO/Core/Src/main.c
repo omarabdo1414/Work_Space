5b81c9e9e15d52ebd227dab3227aa947ca02978c
omarabdo1414 <oabdo6063@gmail.com>
2024-06-02 18:23:22 +0300

    Embedded C++

--- Embedded_C++/GPIO/Core/Src/main.c
+++ Embedded_C++/GPIO/Core/Src/main.c
@@ -1,16 +1,19 @@
 #include "main.h"
-
+#define PIN_NUMBER 2
 
 int main(void)
 {
-	RCC->APB2ENR = (1 << 4);
-	GPIOC->CRH = 0x00300000; // pin 13
+	RCC->APB2ENR =(1 << 2);
+//	GPIOC->CRH = 0x00300000; // pin 13
+	GPIOA->CRL = (0X3 << 4 * PIN_NUMBER);// 0x00000030
 
   while (1)
   {
-	  GPIOC->ODR = (1 << 13);
+//	  GPIOC->ODR = (1 << 13);
+	  GPIOA->ODR = (1 << PIN_NUMBER);
 	  for(int i = 0; i < 100000; i++);
-	  GPIOC->ODR &=~(1 << 13);
+//	  GPIOC->ODR &=~(1 << 13);
+	  GPIOA->ODR &=~ (1 << PIN_NUMBER);
 	  for(int i = 0; i < 100000; i++);
   }
 
